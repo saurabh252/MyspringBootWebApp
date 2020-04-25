99a292f40e1d014b48c7e2f6dbff099bf4974df7
saurabh252 <saurabh123@yahoo.com>
2020-04-26 01:47:32 +0530

    Initial commit

---
+++ src/main/java/com/saurabh/MyspringBootWebApp/MyspringBootWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.saurabh.MyspringBootWebApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyspringBootWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyspringBootWebAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/saurabh/MyspringBootWebApp/MyspringBootWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saurabh.MyspringBootWebApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyspringBootWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
